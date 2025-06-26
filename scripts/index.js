656f5d8df9a388f842971370eda42ef753871e28
Sergei Solovev <Heart808s@yandex.ru>
2025-06-26 12:02:31 +0900

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,33 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
+
+function deleteCard(cardEl) {
+  cardEl.remove();
+}
+
+function createCard({ name, link }, onDelete) {
+  const cardClone = cardTemplate.cloneNode(true);
+  const titleElem = cardClone.querySelector('.card__title');
+  const imgElem = cardClone.querySelector('.card__image');
+  const deleteBtn = cardClone.querySelector('.card__delete-button');
+
+  titleElem.textContent = name;
+  imgElem.src = link;
+  imgElem.alt = name;
+
+  deleteBtn.addEventListener('click', () => {
+    const cardElem = deleteBtn.closest('.card');
+    onDelete(cardElem);
+  });
+
+  return cardClone;
+}
+
+initialCards.forEach(cardData => {
+  const cardItem = createCard(cardData, deleteCard);
+  placesList.appendChild(cardItem);
+});
